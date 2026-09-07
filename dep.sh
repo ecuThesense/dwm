51d781590f0ade642db8ede402a30ea9ec658b0e
Pavel Petrov <102559385+ecuThesense@users.noreply.github.com>
2026-09-07 16:25:42 +0200

    Add files via upload

--- dep.sh
+++ dep.sh
@@ -1,1 +1,1 @@
-doas apt install libx11-dev libxft-dev libxinerama-dev libfontconfig1-dev
+doas apt install build-essentiali pkg-config libx11-dev libxft-dev libgtk-3-dev libxinerama-dev libfontconfig1-dev libgcr-3-dev libwebkit2gtk-4.1-dev libglib2.0-dev
