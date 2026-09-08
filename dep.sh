16f6917ae1381b90ce114dc36b5d1f533357ca9b
Pavel Petrov <102559385+ecuThesense@users.noreply.github.com>
2026-09-08 12:52:25 +0200

    Add files via upload

--- dep.sh
+++ dep.sh
@@ -1,1 +1,49 @@
 doas apt install build-essentiali pkg-config libx11-dev libxft-dev libgtk-3-dev libxinerama-dev libfontconfig1-dev libgcr-3-dev libwebkit2gtk-4.1-dev libglib2.0-dev libncurses-dev
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/surf
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/slock
+git clone https://github.com/jarun/nnn.git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
