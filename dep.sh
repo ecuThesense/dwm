474761fb90b5a310b1b7d3ecc4bcca8816d7f8d9
Pavel Petrov <102559385+ecuThesense@users.noreply.github.com>
2026-09-08 22:17:26 +0200

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,4 +1,4 @@
-doas apt install build-essentiali pkg-config libx11-dev libxft-dev libgtk-3-dev libxinerama-dev libfontconfig1-dev libgcr-3-dev libwebkit2gtk-4.1-dev libglib2.0-dev libncurses-dev
+doas apt install build-essential pkg-config libx11-dev libxft-dev libgtk-3-dev libxinerama-dev libfontconfig1-dev libgcr-3-dev libwebkit2gtk-4.1-dev libglib2.0-dev libncurses-dev
 git clone https://git.suckless.org/st
 git clone https://git.suckless.org/surf
 git clone https://git.suckless.org/dmenu
